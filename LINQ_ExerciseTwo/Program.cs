1a9b3f3bf22020c0c914ab6b42539f01fa136d98
Amr Yousry <amryousry19@gmail.com>
2022-08-25 15:46:17 -0400

    LINQ Exercise Two

---
+++ LINQ_ExerciseTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LINQ_ExerciseTwo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>() 
+            {
+                "God Of War",
+                "Call of Duty",
+                "GTA",
+                "Witcher"
+            };
+
+            var orderedLength = videoGames.OrderBy(x => x.Length).ToList();
+
+            foreach(var videoGame in orderedLength) 
+            {
+                Console.WriteLine(videoGame);
+            } 
+
+        }
+    }
+}
